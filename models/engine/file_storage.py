cf7a5d41eb0c576b962040268a6b1a1f367c6be5
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-30 20:41:42 -0500

    [Feature] add delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,20 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls:
+            object_by_class = {}
+            for key in self.__objects:
+                class_name = key.split(".")
+                if class_name[0] == cls.__name__:
+                    object_by_class.update({key: self.__objects[key]})
+            return object_by_class
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +63,9 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects.pop(key)
