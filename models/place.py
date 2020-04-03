8cd25bc5e14ba2e9e8eeadf17189fbd166679393
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-03 11:01:43 -0500

    [Fix] if class exist

--- models/place.py
+++ models/place.py
@@ -77,5 +77,6 @@ class Place(BaseModel, Base):
         def amenities(self, amenity_obj):
             """handles append method for adding an
             Amenity.id to the attribute amenity_ids"""
-            if isinstance(amenity_obj, models.Amenity):
-                self.amenities.append(amenity_obj.id)
+            str_obj = amenity_obj.__class__.__name__
+            if str_obj == "Amenity":
+                self.amenity_ids.append(str(amenity_obj.id))
