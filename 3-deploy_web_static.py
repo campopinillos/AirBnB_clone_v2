96aed7813bc1e44d66498d02e7e334c53d082d40
campopinillos <1207@holbertonschool.com>
2020-04-09 16:29:38 -0500

    [Fix] Deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,18 +25,21 @@ def do_deploy(archive_path):
     """Deploy"""
     if not path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    dir = "/data/web_static/releases/" + file.split(".")[0]
-    put(archive_path, "/tmp")
-    run("sudo mkdir -p {}".format(dir))
-    run("sudo tar -xzf /tmp/{} -C {}".format(file, dir))
-    run("sudo rm /tmp/{}".format(file))
-    run("sudo mv {}/web_static/* {}/".format(dir, dir))
-    run("sudo rm -rf {}/web_static".format(dir))
-    run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s {} /data/web_static/current".format(dir))
-    print("New version deployed!")
-    return True
+    try:
+        file = archive_path.split("/")[-1]
+        dir = "/data/web_static/releases/" + file.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(dir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(file, dir))
+        run("sudo rm /tmp/{}".format(file))
+        run("sudo mv {}/web_static/* {}/".format(dir, dir))
+        run("sudo rm -rf {}/web_static".format(dir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} /data/web_static/current".format(dir))
+        print("New version deployed!")
+        return True
+    except:
+        return False
 
 
 def deploy():
