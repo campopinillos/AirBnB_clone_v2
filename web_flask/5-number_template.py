e378feb8ef6b90e489cb7f60e61c61909b02625c
campopinillos <1207@holbertonschool.com>
2020-04-21 18:06:47 -0500

    [Fix] Docstring

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,6 +40,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """Number template"""
     return render_template('5-number.html', n=n)
 
 
